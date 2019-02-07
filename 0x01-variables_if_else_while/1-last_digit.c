020a21f3d54844e23f6ab406edd49a1fe30234fc
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-07 22:54:54 +0000

    Corrected trailing withespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,9 +22,9 @@ int main(void)
 	printf("Last digit of %d is %d ", n, last);
 	if (n > 5)
 		printf("and is greater than 5\n");
-			if (n == 0)
-				printf("and is 0\n");
-					if (n < 6 && n != 0)
-						printf("and is less than 6 and not 0\n"); 
-return (0);
+	if (n == 0)
+		printf("and is 0\n");
+	if (n < 6 && n != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
 }
