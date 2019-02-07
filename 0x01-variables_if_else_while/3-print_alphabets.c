464ee52035f3174014f502902940be8ca914b09a
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-07 23:41:20 +0000

    ABC and abc

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a' ; abc <= 'z' ; abc++)
+{
+	putchar(abc);
+}
+	for (abc = 'A' ; abc <= 'Z' ; abc++)
+{
+	putchar(abc);
+}
+	putchar(012);
+	return (0);
+}
