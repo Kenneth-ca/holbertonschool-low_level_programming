86710a2de38e4ccf39dfe41aab156f15f23c2ee8
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-08 00:01:57 +0000

    Numeros con putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numero;
+
+	for (numero = '0' ; numero <= '9' ; numero++)
+{
+		putchar(numero);
+}
+	putchar(012);
+	return (0);
+}
