690832a89372abdc19d66d058bc5b53492059f50
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-13 02:56:55 +0000

    Sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students
+ * @a: argument for isalpha function
+ * @b: argument for isalpha function
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b
+return (sum);
+	}
