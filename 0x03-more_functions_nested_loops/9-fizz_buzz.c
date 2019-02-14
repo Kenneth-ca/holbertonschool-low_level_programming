529516430ba42af73fe6e29953fd206ce4e7f4bb
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-14 23:30:10 +0000

    Fizz-Buzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int cont;
+
+for (cont = 1; cont <= 100; cont++)
+{
+	if (cont % 3 == 0)
+	{
+		if (cont % 5 == 0)
+		printf("FizzBuzz ");
+		else
+		printf("Fizz ");
+	}
+	else if (cont % 5 == 0)
+		printf("Buzz ");
+	else
+		printf("%d ", cont);
+}
+printf("\n");
+return (0);
+}
