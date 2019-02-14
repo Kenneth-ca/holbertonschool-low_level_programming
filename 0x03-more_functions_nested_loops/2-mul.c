a0f7935a01ef61caf1cf59ab6e9add25c57e04fe
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-14 20:25:53 +0000

    Return can take operations

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students
+ * @a: argument for the function
+ * @b: argument for the function
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+return (a*b);
+}
