cdd0c7b16ba7c94a285363ef775935a5e744e177
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-19 01:03:48 +0000

    Changing values of the integers

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code for Holberton School students
+ * @a: argument for the function
+ * @b: argument for the function
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
