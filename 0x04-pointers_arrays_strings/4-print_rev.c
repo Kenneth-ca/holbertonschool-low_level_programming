7f5f74dec3ad634fe3efe61350a70c001db9cffe
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-19 21:01:22 +0000

    String in reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_rev - check the code for Holberton School students
+ * @s: argument for the function
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int i, len;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+len = i;
+}
+for (i = len; i >= 0; i--)
+{
+_putchar(*(s + i));
+}
+_putchar(10);
+}
