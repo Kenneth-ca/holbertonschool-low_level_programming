180b9d33b58e744fbf466d181d73535f92a90315
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-20 02:40:00 +0000

    Add something in the comments

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,8 @@
 
 /**
  * puts_half - check the code for Holberton School students
- * @str: argument for the function
+ *
+ *@str: argument for the function
  *
  * Return: Always 0.
  */
@@ -14,12 +15,10 @@ i = 0;
 	while (str[i] != '\0')
 		i++;
 	if (i % 2 == 0)
-	n = i / 2;
+		n = i / 2;
 	else
-	n = (i - 1) / 2;
+		n = (i - 1) / 2;
 	for (i = n; str[i] != '\0'; i++)
-	{
 		_putchar(*(str + i));
-	}
 _putchar(10);
 }
