7316be2f4b4b0b8e381bf5767bdc81ce84a48a0f
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-20 04:58:26 +0000

    Working on

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code for Holberton School students
+ *
+ *@dest: argument for the function
+ *@src: argument for the function
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+*dest = *src;
+printf("\n");
+return (0);
+}
