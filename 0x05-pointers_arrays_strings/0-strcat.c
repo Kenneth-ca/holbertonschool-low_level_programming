61dab27a8e346b7db32d5e42b385439cd070acd2
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-21 18:57:42 +0000

    All files created, task 0 added

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - check the code for Holberton School students.
+ *
+ * @dest: argument for the function.
+ * @src: argument for the function.
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, lenght = 0;
+	while (dest[lenght] != '\0')
+		lenght++;
+	for (i = 0; dest[i] != '\0'; i++)
+		dest[lenght + i] = src[i];
+dest[lenght + i] =  src[i];
+return (dest);
+}
---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - check the code for Holberton School students.
+ *
+ * @dest: argument for the function.
+ * @src: argument for the function.
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+    return (0);
+}
