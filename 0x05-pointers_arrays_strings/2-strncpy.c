783dc6e6133b1a263c7fe01b8bf6a929285b6474
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-21 20:04:29 +0000

    A function that capies a string

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -2,14 +2,26 @@
 #include <stdio.h>
 
 /**
- * *_strcat - check the code for Holberton School students.
+ * *_strncpy - check the code for Holberton School students.
  *
  * @dest: argument for the function.
  * @src: argument for the function.
+ * @n: argument for the function.
  *
  * Return: Always 0.
  */
-char *_strcat(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
-    return (0);
+int i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';	
+		i++;
+	}
+return (dest);
 }
