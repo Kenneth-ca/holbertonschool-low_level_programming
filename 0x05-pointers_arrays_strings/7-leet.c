1603a04abf0e9c61ac836c030f427cced645ccc3
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-22 01:48:41 +0000

    Created two arrays and put one more loop

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -10,19 +10,14 @@
  */
 char *leet(char *s)
 {
-int i;
+int i, k;
+char letter[] = "aAeEoOtTlL";
+char number[] = "4433007711";
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 97 || s[i] == 65)
-			s[i] = 52;
-		else if (s[i] == 101 || s[i] == 69)
-			s[i] = 51;
-		else if (s[i] == 111 || s[i] == 79)
-			s[i] = 48;
-		else if (s[i] == 116 || s[i] == 84)
-			s[i] = 55;
-		else if (s[i] == 108 || s[i] == 76)
-			s[i] = 49;
+		for (k = 0; letter[k] != '\0'; k++)
+			if (s[i] == letter[k])
+				s[i] = number[k];
 	}
 return (s);
 }
