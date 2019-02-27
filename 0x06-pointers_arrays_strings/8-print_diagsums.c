60c5601bf3cfcb6fc1c69574d34b0634b6289662
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-27 01:33:37 +0000

    Edit condition of first for

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * *_strpbrk - prints buffer in hexa
+ * print_diagsums - prints buffer in hexa
  * @a: argument string
  * @size: size of the array
  *
@@ -11,11 +11,12 @@
 void print_diagsums(int *a, int size)
 {
 	int i, all, sum, sum1;
+
 	sum = 0;
 	sum1 = 0;
 	all = size * size;
 
-	for (i = 0; i < all - 1; i += (size + 1))
+	for (i = 0; i < all; i += (size + 1))
 		sum = sum + a[i];
 	i = 0;
 	for (i = size - 1; i < all - 1; i += (size - 1))
