0bea0523007cb9ec25188fb57cc2db73fef19cbf
Kenneth-ca <kenneth.ca95@gmail.com>
2019-02-28 03:03:33 +0000

    Description

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
+ * raiz - Do the square roo.t
  *
  * @n: Argument for the function.
+ * @i: Argument for the function.
  *
- * Return: 1 if is a prime number otherwise returns 0.
+ * Return: 1 returns i or -1 depending which.
  */
 int raiz(int n, int i)
 {
@@ -18,11 +19,18 @@ int raiz(int n, int i)
 		return (-1);
 	else
 		return (raiz(n, i + 1));
-	
 }
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ *
+ * @n: Argument for the function.
+ *
+ * Return: 1 if is a prime number otherwise returns 0.
+ */
 int _sqrt_recursion(int n)
 {
 	int result;
+
 	result = raiz(n, 1);
 	return (result);
 }
