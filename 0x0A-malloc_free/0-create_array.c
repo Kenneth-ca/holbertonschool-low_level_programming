39e5d931c6ddd517362ffc006874b52f8d7105a7
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-05 21:18:11 +0000

    Using malloc to asing chars to a string

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @c: argument for the function
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int i = 0;
+char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = malloc((size + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);	
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+return (s);
+}
