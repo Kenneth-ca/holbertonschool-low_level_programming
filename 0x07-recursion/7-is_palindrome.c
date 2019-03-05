7b97a11d1c7f8f0143c2850be536e2939dff7a4e
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-05 20:06:23 +0000

    Starting new project

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * palindromo - Do the square roo.t
+ *
+ * @n: Argument for the function.
+ * @i: Argument for the function.
+ *
+ * Return: 1 returns i or -1 depending which.
+ */
+int palindromo(char *s, int i)
+{
+}
+/**
+ * reverse - Do the square roo.t
+ *
+ * @n: Argument for the function.
+ * @i: Argument for the function.
+ *
+ * Return: 1 returns i or -1 depending which.
+ */
+int reverse(char *s, char *rev)
+{
+}
+/**
+ * is_palindrome - Returns the natural square root of a number.
+ *
+ * @s: Argument for the function.
+ *
+ * Return: 1 returns result.
+ */
+int is_palindrome(char *s)
+{
+	int result;
+	char rev
+
+	result = raiz(n, 1);
+	return (result);
+}
