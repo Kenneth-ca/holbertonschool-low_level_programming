5de42527932bc62ea9e13035fa2d9815c0f0991e
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-07 20:57:54 +0000

    Add conditions

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -15,12 +15,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *s;
 int i;
 unsigned int j = 0;
+unsigned int size = 0;
 
-s = malloc(sizeof(s1) + n + 1);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	s = malloc((sizeof(s1) + sizeof(s2)) * sizeof(char) - 1);
 	if (s == NULL)
 	{
 		free(s);
 		return (NULL);
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		size++;
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
@@ -28,7 +37,8 @@ s = malloc(sizeof(s1) + n + 1);
 	}
 	while (j < n)
 	{
-		s[i] = s2[j];
+		if (n < size)
+			s[i] = s2[j];
 		j++;
 		i++;
 	}
