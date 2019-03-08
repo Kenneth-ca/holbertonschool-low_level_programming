6d6bce10b6ad88f9d157f684bac5b636795a47ca
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-08 01:54:39 +0000

    How the memory works (pointes)

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory using malloc
+ * @nmemb: argument for the function
+ * @size: argument for the function
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+	return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+{
+	free(p);
+	return (NULL);
+}
+for (i = 0; i < (nmemb * size); i++)
+	*(p + i) = 0;
+return (p);
+}
