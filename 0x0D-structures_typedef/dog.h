36a98f3e5fd32f90b4107faabd538543b29b01c1
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-11 22:16:04 +0000

    Header file also define a struct dog

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - basic info for dogs
+* @name: first member
+* @age: second member
+* @owner: third member
+*
+* Longer description
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
