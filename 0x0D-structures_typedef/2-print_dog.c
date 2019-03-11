a9538d125436985e4b1660fa1142756fbed887ad
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-11 23:31:53 +0000

    Prints a structure

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - a function that prints the structure
+ *
+ * @d: structure dog contains all the basic info
+ *
+ * return - nothing.
+**/
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+	return;
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
