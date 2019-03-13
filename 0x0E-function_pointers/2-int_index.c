7cb382ac577db32e58c3dd8af3b3f4ece70e5703
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-13 19:59:28 +0000

    Add condition for arguments entry

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,18 +12,21 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i, compare = 0;
-if (size <= 0)
-	return (-1);
-for (i = 0; i < size; i++)
+if (array && size && cmp)
 {
-	compare = (*cmp)(array[i]);
-	if (compare != 0)
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		break;
+		compare = (*cmp)(array[i]);
+		if (compare != 0)
+		{
+			break;
+		}
 	}
+	if (compare != 0)
+		return (i);
+	else
+		return (-1);
 }
-if (compare != 0)
-	return (i);
-else
-	return (-1);
 }
