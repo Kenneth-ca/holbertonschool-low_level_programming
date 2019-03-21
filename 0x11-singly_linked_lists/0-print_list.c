c856b192cac4497d784414953ad9241db16c54f5
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-21 22:58:11 +0000

    Prints the elements of a list

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list.
+ * @h: a list that enters to the function.
+ *
+ * Return: A size.
+ */
+size_t print_list(const list_t *h)
+{
+int length;
+
+while (h != NULL)
+{
+	printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	length++;
+}
+return (length);
+}
