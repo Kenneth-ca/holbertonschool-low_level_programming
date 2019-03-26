1b32b8c441be5f555fcf0b3f33b3fbd5ee0224d6
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-26 19:56:34 +0000

    Change free of temp for head

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -12,10 +12,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	temp = head;
-	while (temp != NULL)
+	while (head != NULL)
 	{
+		temp = head;
 		free(temp);
-		temp = temp->next;
+		head = head->next;
 	}
 }
