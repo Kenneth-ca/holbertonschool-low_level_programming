f8b3582c7e8722e392e8c15feab51685cf0f14d2
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-28 22:08:28 +0000

    Scrapping + function that converts a binary number to unsigned int

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint -
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number = 0, pow = 1, num = 0;
+	int length;
+
+	if (b == NULL)
+		return (0);
+	for (length = 0; b[length] != '\0'; length++)
+	{
+	}
+	while (length--)
+	{
+		if (b[length] == '0' || b[length] == '1')
+		{
+			num = b[length] - '0';
+			number = number + num * pow;
+			pow = pow * 2;
+		}
+		else
+			return (0);
+	}
+	return (number);
+}
---
+++ 0x13-bit_manipulation/example.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint -
+ *
+ * Return: 
+ */
+int main (void)
+{
+	unsigned int n, number;
+
+	n = 101110;
+	printf("%d\n", n);
+	number = n >> 1;
+	printf("%d\n", number);
+}
