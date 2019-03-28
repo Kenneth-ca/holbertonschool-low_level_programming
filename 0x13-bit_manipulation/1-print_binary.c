628f97fc09907eb2e10f4cabd9fa75f5fe864059
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-28 23:29:54 +0000

    A function that converts into binary

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,11 +1,31 @@
 #include "holberton.h"
 
 /**
- * print_binary -
+ * recursion - converts to binary recursively
+ * @n: the number to be converted
  *
- * Return: 
+ * Return: nothing.
+ */
+
+void recursion(unsigned long int n)
+{
+	if (n != 0)
+	{
+		recursion(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
+
+/**
+ * print_binary - converts to binary
+ * @n: the number to be converted
+ *
+ * Return: nothing.
  */
 void print_binary(unsigned long int n)
 {
-
+	if (n == 0)
+		_putchar ('0');
+	else
+		recursion(n);
 }
