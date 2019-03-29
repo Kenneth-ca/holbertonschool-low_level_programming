c81cd02e78397b1d3825f63c7555f34b6bc8cbd4
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-29 01:47:01 +0000

    A function that put 0 at a given index

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
 /**
- * clear_bit -
+ * clear_bit - sets to 0 at index
+ * @n: a number that enters the function
+ * @index: the index
  *
- * Return: 
+ * Return: 1 if it worked, -1 if error
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-
+	if (index < 64)
+	{
+		*n = *n & ~(1 << index);
+		return (1);
+	}
+	return (-1);
 }
