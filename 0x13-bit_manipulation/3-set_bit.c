347ef3ea14da13c571efed3e114bd0c1fa077e7d
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-29 01:34:25 +0000

    A function that sets a number at a given index position

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
 /**
- * set_bit -
+ * set_bit - a function that sets the value of a bit to 1
+ * @n: a number that enters the funtion
+ * @index: the index
  *
- * Return: 
+ * Return: 1 if it worked, or -1 if error
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-
+	if (index < 64)
+	{
+		*n = *n | (1 << index);
+		return (1);
+	}
+	return (-1);
 }
