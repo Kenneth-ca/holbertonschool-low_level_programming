9c32b4097b41ebfc8eb27917270e161b3638e65d
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-29 02:32:11 +0000

    Little indian

--- 0x13-bit_manipulation/100-get_endianness.c
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
 
 /**
- * get_endianness -
+ * get_endianness - checks the endianness
  *
- * Return: 
+ * Return: 0 if big indian, 1 if little indian.
  */
 int get_endianness(void)
 {
+	unsigned int i = 1;
+	char *indian = (char *) &i;
 
+	return (*indian ? 1 : 0);
 }
