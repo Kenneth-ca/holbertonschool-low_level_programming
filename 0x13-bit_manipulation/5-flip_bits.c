034154c147365ad352c7609a2c90a0474cf79976
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-29 02:00:52 +0000

    A function that put the number of bits needed to flip a number to other

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,25 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
+ * flip_bits - will flip a number to get another
+ * @n: the first number
+ * @m: the second number
  *
- * Return: 
+ * Return: the number of bits needed to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int number, i, var, result = 0;
 
+	number = n ^ m;
+	for (i = 0; number != 0; i++)
+	{
+		var = number & 1;
+		number = number >> 1;
+		if (var == 1)
+		{
+			result++;
+		}
+	}
+	return (result);
 }
