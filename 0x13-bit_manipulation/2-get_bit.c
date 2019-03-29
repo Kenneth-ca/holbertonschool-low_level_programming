18dd6871d458ad628365e6506b1eb2d0ff0f5b92
Kenneth-ca <kenneth.ca95@gmail.com>
2019-03-29 01:15:48 +0000

    A function that searches based on an index

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * get_bit -
+ * get_bit - return the value of a bit given an index
+ * @n: the input number
+ * @index: the index
  *
- * Return: 
+ * Return: return the value selected by index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-
+	if (index < 64)
+	{
+		return (n >> index & 1);
+	}
+	return (-1);
 }
