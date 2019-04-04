933465d4d1fc83644dd902fca7fa8727c3403e68
Kenneth-ca <kenneth.ca95@gmail.com>
2019-04-04 20:54:54 +0000

    A function that copies content of a file to another

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -6,43 +6,73 @@
 #include "holberton.h"
 
 /**
- * cp check the code for Holberton School students.
+ * cp - copy from a file to another.
+ * @origin: origin filename
+ * @destiny: destiny filename
  *
  * Return: Always 0.
  */
-/**void cp(const char *origin, char *destiny)
+void cp(const char *origin, char *destiny)
 {
-int fd_o, fd_d;
-char buffer[1024];
+	int fd_o, fd_d, rd, wr, cl;
+	char buffer[1024];
 
-if (origin == NULL)
-{
-	dprintf(2, "Error: Can't read from file ");
-	dprintf(2, "NAME_OF_THE_FILE\n");
-	exit (98);
+	fd_o = open(origin, O_RDONLY);
+	if (fd_o == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		origin),
+		exit(98);
+	fd_d = open(destiny, O_WRONLY | O_CREAT | O_TRUNC, 0662);
+	if (fd_o == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+		destiny),
+		exit(98);
+	while (wr != 0)
+	{
+		rd = read(fd_o, buffer, 1024);
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			origin);
+			exit(98);
+		}
+		wr = write(fd_d, buffer, rd);
+		if (wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+			destiny);
+			exit(99);
+		}
+	}
+	cl = close(fd_o);
+	if (cl == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_o),
+		exit(100);
+	cl = close(fd_d);
+	if (cl == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_d),
+		exit(100);
 }
-fd_o = open(origin, O_RDONLY);
-if (fd_o == -1)
-	exit(98);
-fd_d = open(destiny, O_WRONLY);
-if (fd_o == -1)
-	exit(98);
-
-}
-*/
 /**
  * main - check the code for Holberton School students.
- * 
+ * @argc: number of arguments
+ * @argv: the argument
  *
  * Return: Always 0.
  */
 int main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        dprintf(2, "Usage: cp file_from file_to");
-        exit(97);
-    }
-    /*cp(argv[1], argv[2]);*/
-    return (0);
+	if (argc != 3)
+	{
+	dprintf(2, "Usage: cp file_from file_to\n");
+	exit(97);
+	}
+	if (argv[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		argv[1]);
+		exit(98);
+	}
+	cp(argv[1], argv[2]);
+	return (0);
 }
