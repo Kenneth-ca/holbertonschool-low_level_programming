4223cbf47f03211e3813ecd845b52c76e22b4cab
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-05 02:57:43 +0000

    Frees a list

--- 0x16-doubly_linked_lists/4-free_dlistint.c
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * free_dlistint -
+ * free_dlistint - frees a list
+ * @head: a pointer to a list
  *
- * Return: 
+ * Return: nothing.
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp;
 
+	while (head != NULL)
+	{
+		temp = head;
+		free(temp);
+		head = head->next;
+	}
 }
