118575488b3f19d86bd2dc201e96a0f8bd76c9ee
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-31 22:13:56 +0000

    [Feature] each slot initialized with NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,11 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -
+ * hash_table_create - a function that creates a hash table
+ * @size: the size of the table
  *
- * Return: 
+ * Return: a pointer to a hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new_hash;
+	unsigned long int i = 0;
 
+	new_hash = malloc(sizeof(hash_table_t));
+	if (new_hash == NULL)
+	{
+		return (NULL);
+	}
+	new_hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_hash->array == NULL)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		new_hash->array[i] = NULL;
+		i++;
+	}
+	new_hash->size = size;
+
+	return (new_hash);
 }
