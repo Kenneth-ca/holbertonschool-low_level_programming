0668528e0fdbb4bcfb50a6e70fe72e65b87fa486
Kenneth-ca <kenneth.ca95@gmail.com>
2019-07-06 02:15:05 +0000

    Is it an island?

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,34 +7,42 @@ def island_perimeter(grid):
 
     perimeter = 0
     for i in range(len(grid)):
+        only = 0
         for j in range(len(grid[i])):
             clear = 0
             perimeter += 4
             if (grid[i][j] == 1):
+                only = 1
                 try:
                     if (grid[i + 1][j] == 1):
                         perimeter -= 1
                         clear += 1
+                        only += 1
                 except:
-                    pass
+                    return 0
                 try:
                     if (grid[i - 1][j] == 1):
                         perimeter -= 1
                         clear += 1
+                        only += 1
                 except:
-                    pass
+                    return 0
                 try:
                     if (grid[i][j + 1] == 1):
                         perimeter -= 1
                         clear += 1
+                        only += 1
                 except:
-                    pass
+                    return 0
                 try:
                     if (grid[i][j - 1] == 1):
                         perimeter -= 1
                         clear += 1
+                        only += 1
                 except:
-                    pass
+                    return 0
             if (clear == 0):
                 perimeter -= 4
+        if (only == 1):
+            return 4
     return perimeter
