a138e24ae8f7ddf18ab3175f545fb1750c4ad7c5
Kenneth-ca <kenneth.ca95@gmail.com>
2019-07-06 01:43:52 +0000

    Takes into account that island is around water nad isn't water inside

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,37 +8,21 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+            clear = 0
+            perimeter += 4
             if (grid[i][j] == 1):
-                try:
-                    if (grid[i + 1][j] is None):
-                        perimeter += 1
-                    else:
-                        if (grid[i + 1][j] == 0):
-                            perimeter += 1
-                except:
-                    pass
-                try:
-                    if (grid[i - 1][j] is None):
-                        perimeter += 1
-                    else:
-                        if (grid[i - 1][j] == 0):
-                            perimeter += 1
-                except:
-                    pass
-                try:
-                    if (grid[i][j + 1] is None):
-                        perimeter += 1
-                    else:
-                        if (grid[i][j + 1] == 0):
-                            perimeter += 1
-                except:
-                    pass
-                try:
-                    if (grid[i][j - 1] is None):
-                        perimeter += 1
-                    else:
-                        if (grid[i][j - 1] == 0):
-                            perimeter += 1
-                except:
-                    pass
+                if (grid[i + 1][j] == 1):
+                    perimeter -= 1
+                    clear += 1
+                if (grid[i - 1][j] == 1):
+                    perimeter -= 1
+                    clear += 1
+                if (grid[i][j + 1] == 1):
+                    perimeter -= 1
+                    clear += 1
+                if (grid[i][j - 1] == 1):
+                    perimeter -= 1
+                    clear += 1
+            if (clear == 0):
+                perimeter -= 4
     return perimeter
