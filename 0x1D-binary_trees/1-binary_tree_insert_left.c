4e0f8d42dd41f3eef0e680419e4e7e98c062465d
Kenneth-ca <kenneth.ca95@gmail.com>
2019-07-23 01:09:32 +0000

    Creation of nodes + creation of left-child

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,11 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left -
- *
- * Return: 
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: the parent
+ * @value: the value
+ * Return: a pointer.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node_left;
 
+	if (parent == NULL)
+		return (NULL);
+
+	node_left = binary_tree_node(parent, value);
+	if (node_left == NULL)
+		return (NULL);
+	if (parent->left)
+	{
+		parent->left->parent = node_left;
+		node_left->left = parent->left;
+	}
+	parent->left = node_left;
+	return (node_left);
 }
