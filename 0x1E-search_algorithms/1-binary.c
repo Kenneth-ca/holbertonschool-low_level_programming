1a3a202af32b18cb0ef07ea25e40750de657c147
Kenneth-ca <kenneth.ca95@gmail.com>
2019-08-28 20:58:50 +0000

    Corrected condition of while used for printing array

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,7 +16,7 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	while (start < end)
+	while (start <= end)
 	{
 		index = start;
 		printf("Searching in array: ");
