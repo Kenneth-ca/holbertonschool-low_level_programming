8205f6b6b8fc82aacae8165579bb67cdc79500c8
Kenneth-ca <kenneth.ca95@gmail.com>
2019-10-14 03:17:47 +0000

    [fix] first numbers in sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int i, fib = 0, fib_prev = 2, fib_prev_prev = 1, sum = 0;
+	int i, fib = 0, fib_prev = 2, fib_prev_prev = 1, sum = 2;
 
 	for (i = 2; i < 50; i++)
 	{
 		fib = fib_prev + fib_prev_prev;
+		fib_prev_prev = fib_prev;
+		fib_prev = fib;
 		if (fib > 4000000)
 			break;
 		if (fib % 2 == 0)
 			sum = sum + fib;
-		fib_prev_prev = fib_prev;
-		fib_prev = fib;
 	}
 	printf("%d\n", sum);
 	return (0);
