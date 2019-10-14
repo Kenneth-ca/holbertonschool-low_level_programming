d586cdbb98e8200cac6b1510ea7ceffbd3f6aaed
Kenneth-ca <kenneth.ca95@gmail.com>
2019-10-14 00:00:51 +0000

    [fix] spaces

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_times_table - prints the n times table
@@ -10,7 +11,7 @@ void print_times_table(int n)
 {
 	int i = 0, j = 0, mul = 0;
 
-	if (n >= 15 || n <= 0)
+	if (n > 15 || n < 0)
 		return;
 	for (i = 0; i <= n; i++)
 	{
@@ -18,33 +19,26 @@ void print_times_table(int n)
 		{
 			mul = i * j;
 			if (!(mul / 10))
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(mul + '0');
+			}
 			else if (!(mul / 100))
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar((mul / 10) + '0');
 				_putchar((mul % 10) + '0'); }
 			else if (!(mul / 1000))
 			{
+				_putchar(' ');
 				_putchar((mul / 100) + '0');
 				_putchar(((mul / 10) % 10) + '0');
 				_putchar((mul % 10) + '0'); }
 			if (j < n)
-			{
 				_putchar(',');
-				if (!(mul / 10))
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (!(mul / 100))
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-					_putchar(' ');
-			}
 		}
 		_putchar('\n');
 	}
